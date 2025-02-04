a347c9c4033fdb4bebb61003251dbff329239192
Михаил Нечаев <m1ssha@vk.com>
2025-02-04 20:24:54 +0300

    feat: enhance user logging by separating message and command logging functions

--- functions/user_logger.py
+++ functions/user_logger.py
@@ -1,11 +1,15 @@
 import logging
 from aiogram import Dispatcher, types
+from aiogram.filters import Command
 from database import add_user
 
 def register_user_logger(dp: Dispatcher):
     @dp.message()
-    async def log_user(message: types.Message):
-        """Фиксирует всех пользователей, которые пишут боту"""
+    async def log_user_message(message: types.Message):
         user = message.from_user
         await add_user(user.id, user.username or "No username", user.full_name)
-        logging.info(f"Записан новый пользователь: {user.full_name} ({user.id})")
+
+    @dp.message(Command())
+    async def log_user_command(message: types.Message):
+        user = message.from_user
+        await add_user(user.id, user.username or "No username", user.full_name)
