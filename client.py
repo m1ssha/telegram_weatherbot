7495c9f41cabb1550866b9b8aad812d34181bf7f
Михаил Нечаев <m1ssha@vk.com>
2025-02-04 20:08:32 +0300

    refactor: implement automatic command registration for cleaner code structure

--- client.py
+++ client.py
@@ -1,14 +1,13 @@
 import logging
 import os
 import asyncio
+import importlib
+import pkgutil
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 
-from commands import start, help as help_module, weather, forecast, info, subscribe, mysubscriptions, unsubscribe, contact
-from functions import chat_logger, user_logger
-from commands.admin import getsubscriptions, addsubscriptiontouser, admin, answer, chats, users, removesubscription
-
 import database
+from functions import chat_logger, user_logger
 
 load_dotenv(override=True)
 TOKEN = os.getenv("TELEGRAM_API_KEY")
@@ -25,32 +24,25 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
-start.register_start(dp)
-help_module.register_help(dp)
-weather.register_weather(dp)
-forecast.register_forecast(dp)
-info.register_info(dp)
-contact.register_contact(dp)
+def auto_register_commands(dp: Dispatcher, package: str):
+    package_path = package.replace(".", "/")
+    for _, module_name, _ in pkgutil.iter_modules([package_path]):
+        module = importlib.import_module(f"{package}.{module_name}")
+        register_function_name = f"register_{module_name}"
+        
+        if hasattr(module, register_function_name):
+            getattr(module, register_function_name)(dp)
+            print(f"✅ Зарегистрирована команда: {module_name}")
 
-subscribe.register_subscribe(dp)
-mysubscriptions.register_mysubscriptions(dp)
-unsubscribe.register_unsubscribe(dp)
 
-getsubscriptions.register_getsubscriptions(dp)
-addsubscriptiontouser.register_addsubscriptiontouser(dp)
-removesubscription.register_removesubscriptiontouser(dp)
-admin.register_admin(dp)
-answer.register_answer(dp)
-chats.register_chats(dp)
-users.register_users(dp)
+auto_register_commands(dp, "commands")
+auto_register_commands(dp, "commands.admin")
+
 
 async def main():
-    """Основная функция для запуска бота"""
     await database.init_db()
     chat_logger.register_chat_logger(dp)
     user_logger.register_user_logger(dp)
-    await subscribe.init_db()
-    subscribe.schedule_daily_forecasts(bot)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
