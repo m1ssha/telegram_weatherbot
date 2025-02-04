2720e7da110de4dc9676f463d548d76db6831e3b
Михаил Нечаев <m1ssha@vk.com>
2025-02-04 20:34:09 +0300

    убрал баг надеюсь заработает

--- functions/user_logger.py
+++ functions/user_logger.py
@@ -1,15 +1,8 @@
-import logging
 from aiogram import Dispatcher, types
-from aiogram.filters import Command
 from database import add_user
 
 def register_user_logger(dp: Dispatcher):
     @dp.message()
-    async def log_user_message(message: types.Message):
-        user = message.from_user
-        await add_user(user.id, user.username or "No username", user.full_name)
-
-    @dp.message(Command())
-    async def log_user_command(message: types.Message):
+    async def log_user_interaction(message: types.Message):
         user = message.from_user
         await add_user(user.id, user.username or "No username", user.full_name)
